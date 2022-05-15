96e7d445f1df66c38d60a3cb7ee75b7647be0d76
Josue Lubaki <josuelubaki@gmail.com>
2022-05-15 07:32:08 -0400

    Adding cart Cntext & using Context

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,10 +3,11 @@ import React, { Fragment } from 'react';
 import HeaderCartButton from './HeaderCartButton';
 import classes from './Header.module.css';
 import mealsImage from '../../assets/meals.jpg';
+import CartProvider from '../../store/CartProvider';
 
 const Header = (props) => {
 	return (
-		<Fragment>
+		<CartProvider>
 			<header className={classes.header}>
 				<h1>ReactMeals</h1>
 				<HeaderCartButton onClick={props.onShowCart} />
@@ -14,7 +15,7 @@ const Header = (props) => {
 			<div className={classes['main-image']}>
 				<img src={mealsImage} alt='A table full of delicious food' />
 			</div>
-		</Fragment>
+		</CartProvider>
 	);
 };
 
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import classes from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+	const cartContext = useContext(CartContext);
+
+	const numberOfCartItems = cartContext.items.reduce(
+		(currentNumber, item) => {
+			return currentNumber += item.amount
+		},
+		0
+	);
+
 	return (
 		<button className={classes.button} onClick={props.onClick}>
 			<span className={classes.icon}>
 				<CartIcon />
 			</span>
 			<span>Your cart</span>
-			<span className={classes.badge}>3</span>
+			<span className={classes.badge}>{numberOfCartItems}</span>
 		</button>
 	);
 };
