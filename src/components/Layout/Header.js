aff932bc8d6e16a593b432ca7db9fe45efaf2eff
Josue Lubaki <josuelubaki@gmail.com>
2022-05-15 05:33:51 -0400

    adding the Caart Component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 
+import HeaderCartButton from './HeaderCartButton';
 import classes from './Header.module.css';
-
 import mealsImage from '../../assets/meals.jpg';
 
 const Header = (props) => {
@@ -9,7 +9,7 @@ const Header = (props) => {
 		<Fragment>
 			<header className={classes.header}>
 				<h1>ReactMeals</h1>
-				<button>Cart</button>
+				<HeaderCartButton />
 			</header>
 			<div className={classes['main-image']}>
 				<img src={mealsImage} alt='A table full of delicious food' />
