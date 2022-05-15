6f268703e9ed39298a3d8fd29b3df2812ed8d795
Josue Lubaki <josuelubaki@gmail.com>
2022-05-15 09:16:12 -0400

    Making items Removable

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -35,6 +35,29 @@ const cartReducer = (state, action) => {
 			totalAmount: updatedtotalAmount,
 		};
 	}
+	if (action.type === 'REMOVE') {
+		const existingCartItemIndex = state.items.findIndex(
+			(item) => item.id === action.id
+		);
+
+		const existingItem = state.items[existingCartItemIndex];
+
+		const updateTotalAmount = state.totalAmount - existingItem.price;
+
+		let updatedItems;
+		if (existingItem.amount === 1) {
+			updatedItems = state.items.filter((item) => item.id !== action.id);
+		} else {
+			const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+			updatedItems = [...state.items];
+			updatedItems[existingCartItemIndex] = updatedItem;
+		}
+
+		return {
+			items: updatedItems,
+			totalAmount: updateTotalAmount,
+		};
+	}
 	return defaultCartState;
 };
 
