eb993b5f819b9e3b2d10681733f5b0a5e2bd945c
Josue Lubaki <josuelubaki@gmail.com>
2022-05-17 13:22:38 -0400

    fetching Data with useHttp custom Hook

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,37 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import useHttp from '../../hooks/use-http';
 import classes from './AvailableMeals.module.css';
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 
-const DUMMY_MEALS = [
-	{
-		id: 'm1',
-		name: 'Sushi',
-		description: 'Finest fish and veggies',
-		price: 22.99,
-	},
-	{
-		id: 'm2',
-		name: 'Schnitzel',
-		description: 'A german specialty!',
-		price: 16.5,
-	},
-	{
-		id: 'm3',
-		name: 'Barbecue Burger',
-		description: 'American, raw, meaty',
-		price: 12.99,
-	},
-	{
-		id: 'm4',
-		name: 'Green Bowl',
-		description: 'Healthy...and green...',
-		price: 18.99,
-	},
-];
-
 const AvailableMeals = () => {
-	const mealsList = DUMMY_MEALS.map((meal) => (
+	const [meals, setMeals] = useState([]);
+	const { isLoading, error, sendRequest: fetchMeals } = useHttp();
+
+	useEffect(() => {
+		// transformation function
+		const transformData = (mealsData) => {
+			let mealsArray = [];
+
+			for (let key in mealsData) {
+				mealsArray.push({
+					id: key,
+					name: mealsData[key].name,
+					description: mealsData[key].description,
+					price: mealsData[key].price,
+				});
+			}
+
+			setMeals(mealsArray);
+		};
+
+		fetchMeals(
+			{
+				url: 'https://react-movie-http-1a272-default-rtdb.firebaseio.com/meals.json',
+			},
+			transformData
+		);
+	}, [fetchMeals]);
+
+	const mealsList = meals.map((meal) => (
 		<MealItem
 			key={meal.id}
 			id={meal.id}
@@ -40,6 +42,22 @@ const AvailableMeals = () => {
 			price={meal.price}
 		/>
 	));
+
+	if (isLoading) {
+		return (
+			<section className={classes.mealsLoading}>
+				<p>Loading...</p>
+			</section>
+		);
+	}
+
+	if (error) {
+		return (
+			<section className={classes.mealsError}>
+				<p>{error}</p>
+			</section>
+		);
+	}
 
 	return (
 		<section className={classes.meals}>
